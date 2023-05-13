988f1be5967fa5aff83a8cf1ceb187a7e3532497
MelloBirkan <82703050+MelloBirkan@users.noreply.github.com>
2023-05-13 03:07:58 +0000

    base struct

---
+++ main.swift
@@ -0,0 +1,28 @@
+// Write your code below 🏦
+struct Bank {
+  private let password: String
+  private var balance: Double = 0.00
+  static let depositBonusRate: Double = 0.01
+
+  init (password: String) {
+    self.password = password
+  }
+
+  private func isValid(_ enteredPassword: String) -> Bool {
+    if enteredPassword == self.password {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
+    let depositTotal = deposit + (Bank.depositBonusRate * deposit)
+    return depositTotal
+  }
+
+  func makeDeposit(ofAmount depositAmount: Double) {
+    let depositWithBonus = finalDepositWithBonus(fromInitialDeposit: depositAmount)
+    print("Making a deposit of \(depositAmount) with a bonus rate.\nThe final amount deposited is \(depositWithBonus)")
+  }
+}
